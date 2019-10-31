862c70d8a7951fb58ff2b06b2fae63fb3e6e20bd
linhtm <tranlinh265@gmail.com>
2019-10-31 22:34:32 +0900

    create model User

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,12 +1,17 @@
-class User {
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
   var name;
   var avatar;
   var message;
 
-  User(this.name, this.avatar, this.message);
+  User({this.name, this.avatar, this.message});
 
   User.fromJson(Map<String, dynamic> json)
       : name = json["name"],
         avatar = json["picture"],
         message = json["greeting"];
+
+  @override
+  List<Object> get props => [name, avatar, message];
 }
