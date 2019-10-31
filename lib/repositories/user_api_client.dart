431067e80979ba9008abad66c1f4feb2a93da896
linhtm <tranlinh265@gmail.com>
2019-10-31 22:35:01 +0900

    add repository

---
+++ lib/repositories/user_api_client.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:fb_mess_app/model/user.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
+
+class UserApiClient {
+  static const baseUrl = "http://www.json-generator.com";
+  final http.Client httpClient;
+
+  UserApiClient({@required this.httpClient}) : assert(httpClient != null);
+
+  Future<List<User>> fetchChatList() async {
+    final chatListUrl = "$baseUrl/api/get/cezgOWivOq?indent=2";
+
+    final chatListResponse = await this.httpClient.get(chatListUrl);
+
+    if (chatListResponse.statusCode != 200) {
+      throw Exception('Error when getting user list');
+    }
+
+    return jsonDecode(chatListResponse.body) as List<User>;
+  }
+}
---
+++ lib/repositories/user_repository.dart
@@ -0,0 +1,13 @@
+import 'package:fb_mess_app/model/models.dart';
+import 'package:fb_mess_app/repositories/user_api_client.dart';
+import 'dart:async';
+
+class UserRepository {
+  final UserApiClient userApiClient;
+
+  UserRepository({this.userApiClient}) : assert(userApiClient != null);
+
+  Future<List<User>> getChatList() async {
+    return await userApiClient.fetchChatList();
+  }
+}
