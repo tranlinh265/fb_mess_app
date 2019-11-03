3977d47b3f700227cb28a61752676b00ec764bc0
linhtm <tranlinh265@gmail.com>
2019-11-03 23:47:31 +0900

    add isRead, isSent, isBlockNoti, lastMasTime to model User

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -4,14 +4,29 @@ class User extends Equatable {
   final name;
   final avatar;
   final message;
+  final isRead;
+  final isSent;
+  final isBlockNoti;
+  final lastMsgTime;
 
-  User({this.name, this.avatar, this.message});
+  User(
+      {this.name,
+      this.avatar,
+      this.message,
+      this.isRead,
+      this.isSent,
+      this.isBlockNoti,
+      this.lastMsgTime});
 
   static User fromJson(dynamic json) {
     return User(
       name: json["name"],
       avatar: json["picture"],
       message: json["greeting"],
+      isRead: json["isRead"],
+      isSent: json["isSent"],
+      isBlockNoti: json["isBlockNoti"],
+      lastMsgTime: json["lastMsgTime"],
     );
   }
 
